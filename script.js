725e7654a73bc0ad09e3b513f5cde6cae1f1fa6c
David Skold <lemurman384@gmail.com>
2020-11-04 21:03:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var count = 0
+//=====================================
+// p5js Functions
+function setup(){
+  createCanvas(625,375)
+  background(0)
+}
+//--------------------
+function draw(){
+
+}
+//--------------------
+function keyPressed(){
+  background(0)
+}
+//------------------------
+function mousePressed(){
+  circle(mouseX,mouseY,50)
+}
